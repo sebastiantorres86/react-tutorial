84dc9d837c6176ded9350483915c658e32df07a9
sebastiantorres86 <sebastian.torres.mail@gmail.com>
2020-07-05 21:41:08 -0300

    Updates

---
+++ precache-manifest.96f6dea8e50fe4d08c373b29dc696edc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ccce990152b6ada84eb27d5afdd766",
+    "url": "/sebastiantorres86/react-tutorial/index.html"
+  },
+  {
+    "revision": "43b4f5558781b140a975",
+    "url": "/sebastiantorres86/react-tutorial/static/css/main.74295e73.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/sebastiantorres86/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sebastiantorres86/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b4f5558781b140a975",
+    "url": "/sebastiantorres86/react-tutorial/static/js/main.039d9ba9.chunk.js"
+  },
+  {
+    "revision": "c954b20a405ba699b99e",
+    "url": "/sebastiantorres86/react-tutorial/static/js/runtime-main.7fd711af.js"
+  }
+]);
