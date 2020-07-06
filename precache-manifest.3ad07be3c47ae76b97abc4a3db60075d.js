b09016e42c99178bde65a933cb0b0dd6094653a3
sebastiantorres86 <sebastian.torres.mail@gmail.com>
2020-07-05 22:06:58 -0300

    Updates

---
+++ precache-manifest.3ad07be3c47ae76b97abc4a3db60075d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef63a848651333c2baba6cebdde21e23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43b4f5558781b140a975",
+    "url": "/static/css/main.74295e73.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43b4f5558781b140a975",
+    "url": "/static/js/main.039d9ba9.chunk.js"
+  },
+  {
+    "revision": "31de383a84f0b531942d",
+    "url": "/static/js/runtime-main.408e7f6b.js"
+  }
+]);
